97a756fd5b1a3cf8d117b890e05b0ce16089ab39
Patrick Piriou <piriou@gmail.com>
2023-09-29 00:28:57 +0000

    #20 Restructure Root Module

--- main.tf
+++ main.tf
@@ -1,32 +1,11 @@
+#cloud {
+#  organization = "ExamProPatrick"
+#  workspace {
+#    name = "terra-house-superfly"
+#  }
+#}
 
-cloud {
-  organization = "ExamProPatrick"
-  workspace {
-    name = "terra-house-superfly"
-  }
-}
-
-terraform {
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "aws" {
-  # Configuration options
-}
-
-provider "random" {
-  # Configuration options
-}
-#S3 Bucket 
+#Generate random name for AWS bucket
 resource "random_string" "bucket_name" {
   lower            = true
   upper            = false
@@ -35,13 +14,8 @@ resource "random_string" "bucket_name" {
 }
 
 resource "aws_s3_bucket" "example" {
-bucket = random_string.bucket_name.result
-}
-
-output "random_bucket_name_id" {
-    value = random_string.bucket_name.id
-}
-
-output "random_bucket_name_result" {
-    value = random_string.bucket_name.result
+  bucket = random_string.bucket_name.result
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
