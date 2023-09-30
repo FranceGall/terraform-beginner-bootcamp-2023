5b1874030742396ba452e3286e8ecccaf2d5982e
Patrick Piriou <piriou@gmail.com>
2023-09-30 21:01:05 +0000

    #22-configuration-drift

--- variables.tf
+++ variables.tf
@@ -9,3 +9,15 @@ variable "user_uuid" {
     error_message = "User UUID must be in the format of a UUID (e.g., 123e4567-e89b-12d3-a456-426655440000)"
   }
 }
+variable "bucket_name" {
+  description = "Name of S3 bucket"
+  type        = string
+
+  validation {
+    condition     = (
+    length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 &&
+    can(regex("^([a-zA-Z0-9.-]{3,63})$", var.bucket_name))
+    )
+    error_message = "It must be 3-63 characters long and can only contain letters, numbers, hyphens, and periods."
+  }
+}
